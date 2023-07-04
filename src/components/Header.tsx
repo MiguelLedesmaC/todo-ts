a58eea2ef88c4dfa8c7b86105164f7dab62e9050
MiguelLedesmaC <xcorpio27@gmail.com>
2023-07-04 10:41:28 +0200

    first commit

---
+++ src/components/Header.tsx
@@ -0,0 +1,18 @@
+import { type TodoTitle } from '../types'
+import { CreateTodo } from './CreateTodo'
+
+interface Props {
+  onAddTodo: (title: TodoTitle) => void
+}
+
+export const Header: React.FC<Props> = ({ onAddTodo }) => {
+  return (
+    <header className="header">
+      <h1>
+        Todo
+        <img style={{ width: '80px', height: 'auto', marginLeft: '1rem' }} src="https://www.shutterstock.com/image-vector/short-yellow-pencil-realistic-isolated-260nw-2196759167.jpg" />
+      </h1>
+      <CreateTodo saveTodo={onAddTodo} />
+    </header>
+  )
+}
