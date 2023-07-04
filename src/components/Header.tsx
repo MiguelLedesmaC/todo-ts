040b93a42d0ed1dec2db17e2d39a72b6fd5e5ae0
Miguel Ledesma <103437837+MiguelLedesmaC@users.noreply.github.com>
2023-07-04 10:50:36 +0200

    Update Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -10,7 +10,7 @@ export const Header: React.FC<Props> = ({ onAddTodo }) => {
     <header className="header">
       <h1>
         Todo
-        <img style={{ width: '80px', height: 'auto', marginLeft: '1rem' }} src="https://www.shutterstock.com/image-vector/short-yellow-pencil-realistic-isolated-260nw-2196759167.jpg" />
+{/*         <img style={{ width: '80px', height: 'auto', marginLeft: '1rem' }} src="https://www.shutterstock.com/image-vector/short-yellow-pencil-realistic-isolated-260nw-2196759167.jpg" /> */}
       </h1>
       <CreateTodo saveTodo={onAddTodo} />
     </header>
